705c6e5032de917f01fa4002a572ce084c00fb56
Christian Hiland <hilandchristian112@gmail.com>
2022-10-12 10:28:38 -0400

    Create MainActivity.kt

---
+++ app/scr/main/java/com/christian/rune/MainActivity.kt
@@ -0,0 +1,24 @@
+pakeage com.chrsitian.rune
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+
+       view.findNavController().navigate(R.id.)
+    }
+}
