be2c95c09682f9aca97782085ebbff7eb06c9d1b
Christian Hiland <hilandchristian112@gmail.com>
2022-10-12 11:15:28 -0400

    Create aboutpage.kt

---
+++ app/scr/main/java/com/christian/rune/aboutpage.kt
@@ -0,0 +1,27 @@
+package com.chrsitian.rune
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+           ApplicationProvider.getApplicationContext()).apply {
+          setGraph(R.navigation.navigation)
+          setCurrentDestination(R.id.mainFragment)
+    }
+    
+     // Create a graphical homePage for the Home Page fragment
+        val homePage = launchFragmentInContainer<homeFragment>()
+           
+    // Set the NavController property on the fragment
+        homePage.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+}
